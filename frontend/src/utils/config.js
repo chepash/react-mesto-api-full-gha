c28a8e40459b22a1af7e709ce712fb07761cbb87
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-03-29 16:33:46 +0400

    fix: base_url

--- frontend/src/utils/config.js
+++ frontend/src/utils/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://api.chepa.nomoredomains.work/';
+export const BASE_URL = 'https://api.chepa.nomoredomains.work';
 
 export const cardServerApiOptions = {
   baseUrl: BASE_URL,
