636ed89326d3b59123103419f0e0dd48886a00c7
Pavel Cherdantsev <ch3pash@yandex.ru>
2023-03-30 14:20:07 +0400

    feat: update jwt import in config

--- backend/config.js
+++ backend/config.js
@@ -1,13 +1,15 @@
 require('dotenv').config();
 
-const { JWT_SECRET = '3639eb56b2b25ffaa712df68aa92173c5abfab3b4c6f6ea8b42e93c2cbab0124' } = process.env;
+const { NODE_ENV = 'development' } = process.env;
+
+const JWT_SECRET = NODE_ENV === 'production' ? process.env.JWT_SECRET : 'dev-secret';
+
 const { PORT = '3000' } = process.env;
 const { DB_ADDRESS = 'mongodb://127.0.0.1:27017/mestodb' } = process.env;
 
 const corsOptions = {
   origin: [
     'http://localhost:3001',
-    'http://localhost:3000',
     'https://praktikum.tk',
     'http://praktikum.tk',
     'https://chepa.nomoredomains.work',
